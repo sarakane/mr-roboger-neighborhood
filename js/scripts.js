e2277b70a687625c562160d09549c235310cda9e
Sara Kane <kane.sarak@gmail.com>
2020-06-12 10:27:05 -0700

    Add function that takes the user number, loops from 0 to that number, and returns a string of all numbers that were looped through

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+//Business Logic
+function roboNumbers(number) {
+  let returnNumbers = [];
+  for(let i = 0; i <= number; i++){
+    returnNumbers.push(i);
+  }
+  return returnNumbers.toString();
+}
 
 //User Interface Logic
 $(document).ready(function() {
@@ -5,6 +13,7 @@ $(document).ready(function() {
     event.preventDefault();
     const userInput = parseInt($("#user-input").val());
 
-    $("#robogerfied-numbers").text(userInput);
+    const result = roboNumbers(userInput);
+    $("#robogerfied-numbers").text(result);
   });
 });
