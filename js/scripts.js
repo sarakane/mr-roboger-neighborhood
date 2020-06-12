662f8caafa48bfd5030dbb1d18094c32646cc3c7
Sara Kane <kane.sarak@gmail.com>
2020-06-12 10:15:53 -0700

    Add div for output and JavaScript to update text of output

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
+
+//User Interface Logic
 $(document).ready(function() {
   $("form#beep-boop").submit(function (event) {
     event.preventDefault();
     const userInput = parseInt($("#user-input").val());
+
+    $("#robogerfied-numbers").text(userInput);
   });
 });
